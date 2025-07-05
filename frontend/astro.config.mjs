da840d9d5ef1da9d44259ef155b7fc868c981ffd
didos <didier.tolomeaitondji@gmail.com>
2025-07-05 04:15:35 +0100

    output server

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
 export default defineConfig({
+  output: "server",
   adapter: node({
     mode: "standalone", // <-- ajoute cette option
   }),
